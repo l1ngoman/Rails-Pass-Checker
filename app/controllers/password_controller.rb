b3467cd6a515c6269e0ef6035b48fde04e76ab45
learn <learn@learn08.localdomain>
2018-10-30 16:54:34 -0700

    done

--- app/controllers/password_controller.rb
+++ app/controllers/password_controller.rb
@@ -2,20 +2,21 @@ class PasswordController < ApplicationController
   def check
     @user = params[:userid]
     @pass = params[:password]
+    session[:permitted] = false
     if @pass == nil || @user == nil
-      @msg = ""
+      flash[:alert] = ""
     elsif !good_length?(@user) #should be true
-      @msg = "Your username must be at least 6 characters long."
+      flash[:alert] = "Your username must be at least 6 characters long."
     elsif !good_length?(@pass) #should be true
-      @msg = "Your password must be at least 6 characters long."
+      flash[:alert] = "Your password must be at least 6 characters long."
     elsif has_special_char?(@user) #should be false
-      @msg = "Your username cannot contain #, !, or $."
+      flash[:alert] = "Your username cannot contain #, !, or $."
     elsif !has_special_char?(@pass) #should be true
-      @msg = "Your password must contain #, !, or $."
+      flash[:alert] = "Your password must contain #, !, or $."
     elsif @user == @pass
-      @msg = "Your username and password cannot be the same."
+      flash[:alert] = "Your username and password cannot be the same."
     else
-      @msg = "That's a great username/password combo!"
+      redirect_to action: 'welcome'
     end
   end
 
